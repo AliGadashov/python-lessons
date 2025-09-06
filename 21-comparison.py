8512ac684e512ac728cdf6e178df81c172d10201
AliGadashov <gadashovali13@gmail.com>
2025-09-06 15:01:39 +0400

    comparison added

---
+++ 21-comparison.py
@@ -0,0 +1,23 @@
+# username, password => database 
+
+username = "aliqadashov"
+
+x, y, z = 9, 9, 7
+
+result = x == y  # true
+result = x == z  # false
+result = x !=y # false
+
+result = (x > z) # true
+
+result = (x >= y) # true
+
+result = True == 1 # true
+result = False == 1 # false
+
+result = ((True + False + 40 == 41))  # true
+
+result = "aligadshov" == username # false
+result = "aliqadashov" == username # true
+
+print(result)
