2812b1bf9ff25ebbb59ea842f528e51033a98c59
AliGadashov <gadashovali13@gmail.com>
2025-09-06 14:40:48 +0400

    demo assigments

---
+++ 20-demo_assignments.py
@@ -0,0 +1,43 @@
+a, b, c = 3, 9, 7
+
+numbers = 9, 8, 5, 3, 4
+
+## 1- kullanicidan aldiqiniz iki sayinin carpimi ile a, b, c toplaminin farki nedir
+
+# num1 = int(input("Ilk rakami daxil edin: "))
+# num2 = int(input("2-ci rakami daxil edin: "))
+# result1 = num1 * num2
+# print(f"Daxil olunan rakamlarin vurulmasi: {result1}")
+# result2 = a + b + c
+# print(f"a, b, c nin toplami: {result2}")
+# result = result1 - result2
+# print(f"Total result is: {result}")
+
+## 2- c'nin a'ya kalansiz bolumunu hesablayin
+
+# c //= a
+# print(c)
+
+## 3- (a,b,c) toplaminin mod 5'i nedir 
+
+# result = (a + b + c) % 5
+# print(result)
+
+## 4- a'nin b. kuvvetini hesaplayin
+
+# a **= b
+# print(a)
+
+## 5- "a, *b, c = numbers" islemine gore c'nin kupu kacdir  
+ 
+# a, *b, c = numbers
+
+# c**=3
+# print(c)
+
+## 6- "a, *b, c = numbers" islemine gore  b'nin degerlerini toplami kacdir 
+
+# a, *b, c = numbers
+# sonuc = b[0] + b[1]+ b[2]
+# print(b)
+# print(sonuc)
