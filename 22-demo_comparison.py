7dc36bee201e9dcb3b8633b8d3ba71b64b373c31
AliGadashov <gadashovali13@gmail.com>
2025-09-07 13:20:52 +0400

    demo_comparisson

---
+++ 22-demo_comparison.py
@@ -0,0 +1,52 @@
+#--------------------------------------------------------------------------
+
+## 1- kullanicidan alinan iki sayidan hangisi buyukdur 
+
+# num1 = int(input("Ilk rakami giriniz: "))
+# num2 = int(input("2ci rakami giriniz: "))
+
+# result = num1>num2
+
+# print(f"1ci girilen rakam ikinci girilen rakamdan buyukdurmu: {result}")
+
+## 2- Kullanicidan 2 vizenin "%60"ini ve finalin "%40" ini alaraq ortalama hesablayiniz 
+# Eger ortalama degeri 50 ve ustundeyse gecti altindaysa kaldi yazdiriniz
+
+# vize1 = int(input("Ilk vizeni notunuz giriniz: "))
+# vize2 = int(input("2ci vizeni notunuz giriniz: "))
+# final = int(input("Final notunuzu giriniz: "))
+
+# ortalama = ((vize1 + vize2) / 2) * 0.6 + (final * 0.4)
+# result = ortalama >= 50
+
+# print(f"Ortalama notunuz {ortalama} ve sinifi gecme durumunuz {result} ")
+
+## 3- Kullanicidan alinan sayinin tek, cift oldugu durumu yazdirin 
+
+# num1 = int(input("Rakam giriniz: "))
+
+# result = num1 % 2 == 1
+
+# print(f"Girilen rakam tek sayidir: {result}")
+
+## 4- kullanicidan alinan sayinin negatif, pozitif durumunu yazdirin 
+
+# num1 = int(input("Rakam giriniz: "))
+
+# result = num1>0
+
+# print(f"Girilen sayi pozitif sayidir: {result}")
+
+## 5- Kullanicidan adi ve sifresini isteyib dogrulugunu kontrol ediniz 
+
+# email = "aligadashov@gmail.com" 
+# password = "aligadashov"  
+
+# input_email = input("Email giriniz:")
+# input_password = input("Password giriniz:")
+
+# result_of_email = email == input_email
+# result_of_password = password == input_password
+
+# print(f"Girilen mail adresi dogrudur: {result_of_email}")
+# print(f"Girilen password dogrudur: {result_of_password}")
