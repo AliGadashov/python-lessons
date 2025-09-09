df4f1629af93d7460e04769c76f15c1da9e6afdc
AliGadashov <gadashovali13@gmail.com>
2025-09-09 16:38:19 +0400

    ıf elıf and else added

---
+++ if&elif&else.py
@@ -0,0 +1,31 @@
+
+# if 3 > 2: 
+#     print("program calisdi...")
+
+# x = int(input("Rakam giriniz: "))
+
+# if 10 > x and x % 2 == 0:
+#     print("Rakaminiz 10dan kucuk ve cut")
+# else:
+#     print("Rakaminiz 10dan kucuk ve ya cut deyil")
+
+
+# if x < 10:
+
+#     if x % 2 == 0 :
+#         print("Rakaminiz 10dan kucuk ve cut")
+#     else:
+#         print("Rakaminiz cut rakam deyil")
+
+# else:
+#     print("Rakaminiz 10dan buyuk")
+
+num1 = int(input("Birinci rakami giriniz: "))
+num2 = int(input("Ikinci rakami giriniz: "))
+
+if num1 > num2 :
+    print("Birinci rakam daha buyuk")
+elif num2 > num1:
+    print("Ikinci rakam daha buyuk")
+else:
+    print("Beraberler")
