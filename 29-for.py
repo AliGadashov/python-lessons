dac16340037d8a6ff8174f6e17acebded8d11073
AliGadashov <gadashovali13@gmail.com>
2025-09-11 23:50:36 +0400

    for added

---
+++ 29-for.py
@@ -0,0 +1,24 @@
+# list = [1,2,3,4,5]
+
+# for sayi in list:
+#     print(sayi)
+
+# fruits = ["apple", "grape", "cherry"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# name = "Ali Gadashov"
+
+# for letter in name:
+#     print(letter)
+
+# tuple = (6,7,8,9),1,2,3,4,5
+
+# for number in tuple:
+#     print(number)
+
+# dictionary = {"k1": 4, "k2": 5, "k3": 6}
+
+# for key, value in dictionary.items():
+#     print(f"key: {key} \nvalue: {value}")
