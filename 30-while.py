3fa48f35fe16ea95a6deae7c7324171e9fa33a6b
AliGadashov <gadashovali13@gmail.com>
2025-09-12 12:27:56 +0400

    while added

---
+++ 30-while.py
@@ -0,0 +1,20 @@
+# x = 1
+
+# while x <=10:
+#     print(x)
+#     x+=1
+
+# my_list = [1,2 ,3,4,5,6,7,8,9,10]
+
+# x = 0
+
+# while x < len(my_list):
+#     print( my_list[x])
+#     x+=1
+
+# x = 0
+
+# while x<=100:
+#     if x%2 == 0:
+#         print(x)
+#     x+=1
