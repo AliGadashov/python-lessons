b9166044c9add4f4705258ba578408cd01c5fe02
AliGadashov <gadashovali13@gmail.com>
2025-09-16 22:10:57 +0400

    32-break_continue added

---
+++ 32-break_continue.py
@@ -0,0 +1,26 @@
+x = 0 
+
+# while x < 5:
+#     if x == 3 :
+#         break ## dogru durur
+#     x+=1
+#     print(x)
+
+# while x < 5:
+#     if x == 3 :
+#         continue  ## dongu durmuyor 
+#     x+=1
+#     print(x)
+
+while x <5:
+    x+=1
+    if x ==3:
+        continue
+    print(x)
+
+name = "Gadashov Ali"
+
+for i in name:
+    if i == " ":
+        continue
+    print(i) 
