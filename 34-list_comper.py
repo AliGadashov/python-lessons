b14898670f1b43924b5b0a196481195cfd3b49dc
AliGadashov <gadashovali13@gmail.com>
2025-09-18 00:24:46 +0400

    list comper added

---
+++ 34-list_comper.py
@@ -0,0 +1,32 @@
+# numbers= []
+# for i in range(10):
+#     numbers.append(i)
+# print(numbers)
+
+# print("------------------------------------------")
+
+# numbers = [i for i in range(10)]
+# print(numbers)
+
+#-------------------------------------------------------
+
+# numbers = []
+# for i in range(10):
+#     if (i%3 ==0):
+#         print(i**2)
+
+# numbers = [(i**2) for i in range(10) if i % 3 == 0]
+
+# print(numbers)
+
+#-------------------------------------------------------
+
+# myString = "Gadashov Ali"
+
+# myList = []
+
+# for letter in myString:
+#     print(letter)
+
+# myList = [letter for letter in myString]
+# print(myString)
